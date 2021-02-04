90cb4d91983d9f05288f2483f1215580b66505e2
kiiim333 <kiiid78987@gmail.com>
2021-02-04 15:37:29 +0900

    make html tag with js
    make html tag with js for object practice

--- Zerocho/3 끝말잇기/app.js
+++ Zerocho/3 끝말잇기/app.js
@@ -1,11 +1,28 @@
-let word = "제로초";
+//make html tag with js
 
-while (true) {
-  let answer = prompt(word);
-  if (word[word.length - 1] === answer[0]) {
-    alert("Correct!😎");
-    word = answer;
-  } else {
-    alert("Wrong!😫");
-  }
-}
+const body = document.body;
+let word = document.createElement("div");
+word.textContent = "제로초";
+document.body.append(word);
+
+let input = document.createElement("input");
+document.body.append(input);
+
+let button = document.createElement("button");
+button.textContent = "입력!";
+document.body.append(button);
+
+let result = document.createElement("div");
+document.body.append(result);
+
+// let startWord = "제로초";
+
+// while (true) {
+//   let answer = prompt(word);
+//   if (word[word.length - 1] === answer[0]) {
+//     alert("Correct!😎");
+//     word = answer;
+//   } else {
+//     alert("Wrong!😫");
+//   }
+// }
