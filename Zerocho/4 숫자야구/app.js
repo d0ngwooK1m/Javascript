91d8ea3494779c3dd13f867e9f8b5101f7f8eb66
kiiim333 <kiiid78987@gmail.com>
2021-02-04 23:35:49 +0900

    숫자야구
    1. 순서도를 이용해 정리
    2. 배열과 함수 이벤트리스너를 통해 숫자야구 만들기
    3.반복되는 부분 함수로 줄이기

---
+++ Zerocho/4 숫자야구/app.js
@@ -0,0 +1,71 @@
+const body = document.body;
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let numArr = [];
+
+function pickNumber() {
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  numArr = [];
+  for (i = 0; i < 4; i += 1) {
+    let pick = numbers.splice(Math.floor(Math.random() * (9 - i)), 1)[0]; // 마지막에 [0]을 붙이는 이유: 결과 배열이 4개 배열의 묶음으로 나오기 때문에 인자를 뽑아서 넣는 역할.
+    numArr.push(pick);
+  }
+}
+
+console.log(numArr);
+
+const result = document.createElement("h1");
+body.append(result);
+
+const form = document.createElement("form");
+body.append(form);
+
+const input = document.createElement("input");
+input.type = "text";
+input.maxLength = 4; //글자제한 텍스트에서만 적용된다.
+form.append(input);
+
+const button = document.createElement("button");
+button.textContent = "스윙!";
+form.append(button);
+
+let wrong = 0;
+
+form.addEventListener("submit", function asynchronousFunc(e) {
+  e.preventDefault();
+  let answer = input.value;
+  //   console.log(answer, numArr, answer === numArr.join(""));
+  if (Number(answer) === Number(numArr.join(""))) {
+    //답이 맞으면
+    result.textContent = "홈런!";
+    input.focus();
+    pickNumber();
+    wrong = 0;
+  } else {
+    // 답이 틀리면
+    let answerArr = answer.split("");
+    let strike = 0;
+    let ball = 0;
+    wrong += 1;
+    if (wrong > 10) {
+      result.textContent = `10번 넘게 틀려서 실패!😭 답은 ${numArr.join(
+        ""
+      )}였습니다.`;
+      input.value = "";
+      input.focus();
+      pickNumber();
+      wrong = 0;
+    } else {
+      for (i = 0; i <= 3; i += 1) {
+        if (Number(answerArr[i]) === Number(numArr[i])) {
+          strike += 1;
+        } else if (numArr.indexOf(Number(answerArr[i])) > -1) {
+          ball += 1;
+        }
+      }
+      result.textContent = `${strike}스트라이크 ${ball}볼입니다.`;
+      input.value = "";
+      input.focus();
+    }
+  }
+});
