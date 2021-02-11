da0d0376507fcf42244437c217d1d4a316d16eb2
kiiim333 <kiiid78987@gmail.com>
2021-02-11 17:57:28 +0900

    가위바위보3
    자료구조(딕셔너리)를 이용하여 좀 더 알아보기 쉽게 가위바위보 코드를 작성

--- Zerocho/7 가위바위보/app.js
+++ Zerocho/7 가위바위보/app.js
@@ -1,22 +1,37 @@
-let left = 0;
+let computerChoice = 0;
+
+const rsp = {
+  rock: 0,
+  scissor: "-250px",
+  paper: "-500px",
+};
+//적절한 변수명과 딕셔너리 자료구조를 이용하여 사람이 좀 더 이해하기 쉽도록 작성했다.
+
+const rspReverse = {
+  0: rock,
+  "-250px": scissor,
+  "-500px": paper,
+};
+
 setInterval(function () {
-  if (left === 0) {
-    left = "-250px";
-  } else if (left === "-250px") {
-    left = "-500px";
+  if (computerChoice === rsp.rock) {
+    computerChoice = rsp.scissor;
+  } else if (computerChoice === rsp.scissor) {
+    computerChoice = rsp.paper;
   } else {
-    left = 0;
+    computerChoice = rsp.rock;
   }
   document.querySelector(
     "#computer"
-  ).style.background = `url(https://thumb.ac-illust.com/02/028c32e022c0165725eaa79cbeb23e05_w.jpeg) ${left} 0`;
+  ).style.background = `url(https://thumb.ac-illust.com/02/028c32e022c0165725eaa79cbeb23e05_w.jpeg) ${computerChoice} 0`;
 }, 100);
 
 document.querySelectorAll(".btn").forEach(function (btn) {
   //다른 메소드는 for을 이용해서 반복하는 방법도 있는 모양이다.
   //여기서는 querySelectorAll에 forEach(함수)를 넣어서 버튼 마다 함수를 적용
   btn.addEventListener("click", function () {
-    console.log(this.textContent, left);
+    const myChoice = this.textContent;
+    console.log(myChoice, rspReverse[computerChoice]);
     //이벤트 핸들러 안에서 쓴 this는 HTML 요소를 가리킨다. btn의 텍스트 콘텐트를 표시하라는 뜻
   });
 });
