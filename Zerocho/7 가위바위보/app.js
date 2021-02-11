90b65d74bc4e0f6f02d571a8f9e744e74e5152d2
kiiim333 <kiiid78987@gmail.com>
2021-02-11 16:31:59 +0900

    가위바위보1.5
    querySelectorAll 과 forEach를 활용하여 각 버튼에 함수 적용, addEventListener와 this를 활용해서 클릭할 때마다 어떤 버튼인지 알 수 있게 하는 작업

--- Zerocho/7 가위바위보/app.js
+++ Zerocho/7 가위바위보/app.js
@@ -11,3 +11,12 @@ setInterval(function () {
     "#computer"
   ).style.background = `url(https://thumb.ac-illust.com/02/028c32e022c0165725eaa79cbeb23e05_w.jpeg) ${left} 0`;
 }, 100);
+
+document.querySelectorAll(".btn").forEach(function (btn) {
+  //다른 메소드는 for을 이용해서 반복하는 방법도 있는 모양이다.
+  //여기서는 querySelectorAll에 forEach(함수)를 넣어서 버튼 마다 함수를 적용
+  btn.addEventListener("click", function () {
+    console.log(this.textContent, left);
+    //이벤트 핸들러 안에서 쓴 this는 HTML 요소를 가리킨다. btn의 텍스트 콘텐트를 표시하라는 뜻
+  });
+});
