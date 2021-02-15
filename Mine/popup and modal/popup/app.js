7ea366b7e3dcf02a63d07ff9a495e32d6899ab9e
kiiim333 <kiiid78987@gmail.com>
2021-02-15 20:10:31 +0900

    팝업창
    팝업창이 어떤 방식으로 뜨는지 알아봄.

---
+++ Mine/popup and modal/popup/app.js
@@ -0,0 +1,12 @@
+function() {
+    window.open(
+        url,
+        popupName,
+        "width=400, height=600, left=400, top=400, resizable = yes"
+      );
+}
+
+
+addEventListener("load", function () {
+
+});
