0938993b16766f9d06d77d83b1d21034e508f87f
kiiim333 <kiiid78987@gmail.com>
2021-02-15 23:18:30 +0900

    지뢰찾기2
    로또 랜덤 추첨기 부분(피셔 예이츠 셔플)을 활용해서 지뢰가 랜덤한 위치에 깔리게 만들었다. 그러나 가로칸이 한자리 수 일 때 제대로 표현할 방법을 아직 찾지 못함

--- Zerocho/8 지뢰찾기/app.js
+++ Zerocho/8 지뢰찾기/app.js
@@ -5,6 +5,19 @@ document.querySelector("#exec").addEventListener("click", function () {
   console.log(horizontal, vertical, mine);
   //querySelector의 가로 세로 지뢰값을 구하면 그 값을 parseInt로 문자열에서 숫자로 바꾸고, 실행 버튼을 눌렀을 때 console.log에서 확인할 수 있게 한다.
 
+  const balls = Array(horizontal * vertical)
+    .fill()
+    .map(function (element, index) {
+      return index;
+    }); // 이 부분은 for문 없이 꽉 찬 array를 만들고 싶을 때 유용하게 쓰인다. Array fill map의 조합
+
+  const shuffle = [];
+  while (balls.length > horizontal * vertical - mine) {
+    const pick = balls.splice(Math.floor(Math.random() * balls.length), 1)[0];
+    shuffle.push(pick);
+  } //로또 추첨기 활용 지뢰 위치 랜덤 선정 (피셔 예이츠 셔플)
+  console.log(shuffle);
+
   const dataSet = [];
   const tbody = document.querySelector("#table tbody");
   for (i = 0; i < vertical; i += 1) {
@@ -15,10 +28,19 @@ document.querySelector("#exec").addEventListener("click", function () {
       arr.push(1);
       const td = document.createElement("td");
       tr.appendChild(td);
-      td.textContent = 1;
     }
     tbody.appendChild(tr);
   } //반복문이 2번 들어가면서 지뢰찾기 판을 만드는 부분 html구조상 테이블을 만들려면 tr(세로) 다음 td(가로)가 들어가는 구조라 다음처럼 반복문 작성.
   // 2중 반복문 작성 시 원소(i, j)는 다르게 지정 할 것!
-  console.log(dataSet);
+
+  for (k = 0; k < shuffle.length; k += 1) {
+    //59
+    const verticalWhere = Math.floor(shuffle[k] / 10); //59 나누기 10 버림 답5
+    const horizontalWhere = shuffle[k] % 10; //%는 몫으로 나눈 나머지 빼기1 답 8
+    console.log(verticalWhere, horizontalWhere);
+    tbody.children[verticalWhere].children[horizontalWhere].textContent = "X";
+    dataSet[verticalWhere][horizontalWhere] = "X";
+  }
+
+  //   console.log(dataSet);
 });
